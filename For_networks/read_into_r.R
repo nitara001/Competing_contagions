c6706dfa74261d85aacf0294feaf87cd05a8e007
Nitara Wijayatilake <s2607536@ed.ac.uk>
2025-01-08 14:52:32 +0000

    work

--- For_networks/read_into_r.R
+++ For_networks/read_into_r.R
@@ -2,17 +2,10 @@
 library(igraph)
 
 # Define the top-level folder containing the subfolders
-top_level_dir <- "C:\\Users\\s2607536\\Documents\\GitHub\\Competing_contagions\\Networks"
-
-
-# List all subfolders and sub-subfolders
+top_level_dir <- "C:\\Users\\s2607536\\Documents\\GitHub\\Competing_contagions\\Cleaned_Networks"
 all_subfolders <- list.dirs(top_level_dir, full.names = TRUE, recursive = TRUE)
 all_subfolders
-
-# Create a list to store subfolder names and their respective graphs
 all_graphs <- list()
-
-# Loop through each subdirectory
 for (sub in all_subfolders) {
   subfolder_name <- basename(sub)
   graphml_files <- list.files(sub, pattern = ".graphml", full.names = TRUE)
@@ -26,8 +19,7 @@ for (sub in all_subfolders) {
       message(paste("Error reading:", graphml_file))
       return(NULL)  # Return NULL if there’s an error
     })
-    
-    # Only add non-null graphs to the list
+
     if (!is.null(graph)) {
       subfolder_graphs <- c(subfolder_graphs, list(graph))
     }
@@ -35,7 +27,6 @@ for (sub in all_subfolders) {
   
   all_graphs[[subfolder_name]] <- subfolder_graphs
 }
-
 # Extract networks from all_graphs and apply filtering
 extracted_networks <- list()
 for (i in seq_along(all_graphs)) {
